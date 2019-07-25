ba12e90d29f8876bfecd7291384cf4d27143acd9
DmitriiS <dmitris@blue-zoo.co.uk>
2019-07-25 17:21:41 +0100

    Working version

--- Callbacks/biDir/biDir.py
+++ Callbacks/biDir/biDir.py
@@ -19,8 +19,7 @@ class BiDirectionalConstr:
         self.mFirstNode = getMObject('pCone1')
         self.mSecondNode = getMObject('pCube1')
         
-        #callbacksDict = {'firstDirtyplugCBID'   : None,
-        #                 'secondDirtyplugCBID'  : None, 
+        #callbacksDict = {
         #                 'firstAttrChangeCBID'  : None,
         #                 'secondAttrChangeCBID' : None 
         #                }
@@ -29,16 +28,24 @@ class BiDirectionalConstr:
         #    if callbackName in globals().keys():
         #        om2.MMessage.removeCallback(callbacksDict[callbackName])
 
-        firstDirtyplugCBID = om2.MNodeMessage.addNodeDirtyPlugCallback(self.mFirstNode, self.dirtyPlugCBfunc, [self.mFirstNode ,self.mSecondNode])
-        #callbacksDict['firstDirtyplugCBID'] = firstDirtyplugCBID
-        secondDirtyplugCBID = om2.MNodeMessage.addNodeDirtyPlugCallback(self.mSecondNode, self.dirtyPlugCBfunc, [self.mSecondNode ,self.mFirstNode])
-        #callbacksDict['secondDirtyplugCBID'] = secondDirtyplugCBID
+
+        self.updateOffset()
 
         firstAttrChangeCBID = om2.MNodeMessage.addAttributeChangedCallback(self.mFirstNode, self.attrChangedCBfunc, [self.mFirstNode ,self.mSecondNode])
         #callbacksDict['firstAttrChangeCBID'] = firstAttrChangeCBID
         secondAttrChangeCBID = om2.MNodeMessage.addAttributeChangedCallback(self.mSecondNode, self.attrChangedCBfunc, [self.mSecondNode ,self.mFirstNode])
         #callbacksDict['secondAttrChangeCBID'] = secondAttrChangeCBID
 
+    def updateOffset(self):
+        parentPath = om2.MDagPath.getAPathTo(self.mFirstNode)
+        childPath = om2.MDagPath.getAPathTo(self.mSecondNode)
+        
+        #Matricies
+        parentMtx = om2.MDagPath.inclusiveMatrix(parentPath)
+        childMtx = om2.MDagPath.inclusiveMatrix(childPath)
+        self.offset = childMtx * parentMtx.inverse()
+
+    '''
     def dirtyPlugCBfunc(self, node, plug, nodes):  
         #print "dirt", self.driver if self.driver is None else om2.MDagPath.getAPathTo(self.driver).partialPathName(), plug.name()
         #Get parent/child
@@ -58,11 +65,24 @@ class BiDirectionalConstr:
             parentMtx = om2.MDagPath.inclusiveMatrix(parentPath)
             childMtx = om2.MDagPath.inclusiveMatrix(childPath)
             self.offset = childMtx * parentMtx.inverse()
-
+    '''
 
     def attrChangedCBfunc(self, msg, plug, otherPlug, nodes):
-        if self.driver and self.driver == nodes[1]:
-            #print "exiting early from attr change callback"
+        #print "attrChanged", plug.name(), self.driver if not self.driver else om2.MDagPath.getAPathTo(self.driver)
+        if plug.name() == om2.MDagPath.getAPathTo(self.mFirstNode).partialPathName() + ".eval":
+            if mc.getAttr(om2.MDagPath.getAPathTo(self.mFirstNode).partialPathName() + ".eval"):
+                self.updateOffset()
+
+        if not mc.getAttr(om2.MDagPath.getAPathTo(self.mFirstNode).partialPathName() + ".eval"):
+            return
+
+        if not self.driver:
+            self.driver = nodes[0]
+
+        if self.driver == nodes[1]:
+            def a():
+                self.driver = None
+            mc.evalDeferred(a)
             return
 
         if not (msg & om2.MNodeMessage.kAttributeSet):
@@ -79,23 +99,25 @@ class BiDirectionalConstr:
 
         #PostTransform
         parentPostMtx = om2.MDagPath.inclusiveMatrix(parentPath)
-        childPostMtx = self.offset * parentPostMtx
+
+        if self.driver == self.mFirstNode:
+            childPostMtx = self.offset * parentPostMtx
+        else:
+            childPostMtx = self.offset.inverse() * parentPostMtx
 
         transformMtx = om2.MTransformationMatrix(childPostMtx)
-        om2.MFnTransform(child).setTransformation(transformMtx)
-
-        def a():
-            #print "stop driving"
-            self.driver = None
-        mc.evalDeferred(a)
-        #self.driver = None
-        
+        om2.MFnTransform(child).setTransformation(transformMtx)       
+        #translation = transformMtx.translation(om2.MSpace.kWorld)
+        #mc.setAttr(om2.MDagPath.getAPathTo(child).partialPathName() + ".t", *translation)
         
 
 if __name__ == '__main__':
     mc.file(new=1, f=1)
     mc.polyCube()
-    mc.setAttr(mc.polyCone()[0] + ".t", 5, 5, 5)
+    #mc.setAttr(mc.polyCone()[0] + ".t", 5, 5, 5)
+    mc.polyCone()
+    from rigging.functions import *
+    attrFn.addAttr("pCone1", "eval", "long", 1, 0, 1)
     biConstr = BiDirectionalConstr()
     
     
