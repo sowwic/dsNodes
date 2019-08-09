9e19418c1c17c86443d9e9f01e2d9c53ccedec06
DmitriiS <dmitris@blue-zoo.co.uk>
2019-08-09 10:23:28 +0100

    cleanup drawing methods

--- dsDrawVector/plugins/drawVector.py
+++ dsDrawVector/plugins/drawVector.py
@@ -11,62 +11,13 @@ def maya_useNewAPI():
     """
     pass
 
-def matrixAsArray(matrix):
-    array = []
-    for i in range(16):
-        array.append(matrix[i])
-    return array
-
-## Foot Data
-##
-sole = [ [  0.00, 0.0, -0.70 ],
-                 [  0.04, 0.0, -0.69 ],
-                 [  0.09, 0.0, -0.65 ],
-                 [  0.13, 0.0, -0.61 ],
-                 [  0.16, 0.0, -0.54 ],
-                 [  0.17, 0.0, -0.46 ],
-                 [  0.17, 0.0, -0.35 ],
-                 [  0.16, 0.0, -0.25 ],
-                 [  0.15, 0.0, -0.14 ],
-                 [  0.13, 0.0,  0.00 ],
-                 [  0.00, 0.0,  0.00 ],
-                 [ -0.13, 0.0,  0.00 ],
-                 [ -0.15, 0.0, -0.14 ],
-                 [ -0.16, 0.0, -0.25 ],
-                 [ -0.17, 0.0, -0.35 ],
-                 [ -0.17, 0.0, -0.46 ],
-                 [ -0.16, 0.0, -0.54 ],
-                 [ -0.13, 0.0, -0.61 ],
-                 [ -0.09, 0.0, -0.65 ],
-                 [ -0.04, 0.0, -0.69 ],
-                 [ -0.00, 0.0, -0.70 ] ]
-heel = [ [  0.00, 0.0,  0.06 ],
-                 [  0.13, 0.0,  0.06 ],
-                 [  0.14, 0.0,  0.15 ],
-                 [  0.14, 0.0,  0.21 ],
-                 [  0.13, 0.0,  0.25 ],
-                 [  0.11, 0.0,  0.28 ],
-                 [  0.09, 0.0,  0.29 ],
-                 [  0.04, 0.0,  0.30 ],
-                 [  0.00, 0.0,  0.30 ],
-                 [ -0.04, 0.0,  0.30 ],
-                 [ -0.09, 0.0,  0.29 ],
-                 [ -0.11, 0.0,  0.28 ],
-                 [ -0.13, 0.0,  0.25 ],
-                 [ -0.14, 0.0,  0.21 ],
-                 [ -0.14, 0.0,  0.15 ],
-                 [ -0.13, 0.0,  0.06 ],
-                 [ -0.00, 0.0,  0.06 ] ]
-soleCount = 21
-heelCount = 17
-
 class footPrint(omui.MPxLocatorNode):
-    id = om.MTypeId( 0x80408 )
-    drawDbClassification = "drawdb/geometry/footPrint"
-    drawRegistrantId = "FootprintNodePlugin"
+
+    id = om.MTypeId( 0x80067 )
+    drawDbClassification = "drawdb/geometry/vector"
+    drawRegistrantId = "drawVectorPlugin"
 
     size = None## The size of the foot
-
     sourcePt = om.MObject()
     aimPt = om.MObject()
     vector = om.MObject()
@@ -114,12 +65,6 @@ class footPrintData(om.MUserData):
     def __init__(self):
         om.MUserData.__init__(self, False) ## don't delete after draw
 
-        self.fColor = om.MColor()
-        self.fSoleLineList = om.MPointArray()
-        self.fSoleTriangleList = om.MPointArray()
-        self.fHeelLineList = om.MPointArray()
-        self.fHeelTriangleList = om.MPointArray()
-
         ######VECTOR
         self.fsourcePt = om.MPoint()
         self.faimPt = om.MPoint()
@@ -149,20 +94,6 @@ class footPrintDrawOverride(omr.MPxDrawOverride):
     def isBounded(self, objPath, cameraPath):
         return False
 
-    def boundingBox(self, objPath, cameraPath):
-        corner1 = om.MPoint( -0.17, 0.0, -0.7 )
-        corner2 = om.MPoint( 0.17, 0.0, 0.3 )
-
-        multiplier = self.getMultiplier(objPath)
-        corner1 *= multiplier
-        corner2 *= multiplier
-
-        self.mCurrentBoundingBox.clear()
-        self.mCurrentBoundingBox.expand( corner1 )
-        self.mCurrentBoundingBox.expand( corner2 )
-
-        return self.mCurrentBoundingBox
-
     def disableInternalBoundingBoxDraw(self):
         return self.mCustomBoxDraw
 
@@ -176,31 +107,8 @@ class footPrintDrawOverride(omr.MPxDrawOverride):
         global soleCount, sole
         global heelCount, heel
 
-        fMultiplier = self.getMultiplier(objPath)
         fPoints = self.getPoints(objPath)
-
-        data.fSoleLineList.clear()
-        for i in range(soleCount):
-            data.fSoleLineList.append( om.MPoint(sole[i][0] * fMultiplier, sole[i][1] * fMultiplier, sole[i][2] * fMultiplier) )
-
-        data.fHeelLineList.clear()
-        for i in range(heelCount):
-            data.fHeelLineList.append( om.MPoint(heel[i][0] * fMultiplier, heel[i][1] * fMultiplier, heel[i][2] * fMultiplier) )
-
-        data.fSoleTriangleList.clear()
-        for i in range(1,soleCount-1):
-            data.fSoleTriangleList.append( om.MPoint(sole[0][0] * fMultiplier, sole[0][1] * fMultiplier, sole[0][2] * fMultiplier) )
-            data.fSoleTriangleList.append( om.MPoint(sole[i][0] * fMultiplier, sole[i][1] * fMultiplier, sole[i][2] * fMultiplier) )
-            data.fSoleTriangleList.append( om.MPoint(sole[i+1][0] * fMultiplier, sole[i+1][1] * fMultiplier, sole[i+1][2] * fMultiplier) )
-
-        data.fHeelTriangleList.clear()
-        for i in range(1,heelCount-1):
-            data.fHeelTriangleList.append( om.MPoint(heel[0][0] * fMultiplier, heel[0][1] * fMultiplier, heel[0][2] * fMultiplier) )
-            data.fHeelTriangleList.append( om.MPoint(heel[i][0] * fMultiplier, heel[i][1] * fMultiplier, heel[i][2] * fMultiplier) )
-            data.fHeelTriangleList.append( om.MPoint(heel[i+1][0] * fMultiplier, heel[i+1][1] * fMultiplier, heel[i+1][2] * fMultiplier) )
-
         data.fColor = omr.MGeometryUtilities.wireframeColor(objPath)
-
         data.fsourcePt = fPoints[0]
         data.faimPt = fPoints[1]
 
@@ -213,41 +121,14 @@ class footPrintDrawOverride(omr.MPxDrawOverride):
         locatordata = data
         if not isinstance(locatordata, footPrintData):
             return
-
         drawManager.beginDrawable()
         #Draw Vector
-        drawManager.line(locatordata.fsourcePt, locatordata.faimPt)
-
-        ##Draw the foot print solid/wireframe
         drawManager.setColor( locatordata.fColor )
         drawManager.setDepthPriority(5)
-
-        if (frameContext.getDisplayStyle() & omr.MFrameContext.kGouraudShaded):
-            drawManager.mesh(omr.MGeometry.kTriangles, locatordata.fSoleTriangleList)
-            drawManager.mesh(omr.MGeometry.kTriangles, locatordata.fHeelTriangleList)
-
-        drawManager.mesh(omr.MUIDrawManager.kClosedLine, locatordata.fSoleLineList)
-        drawManager.mesh(omr.MUIDrawManager.kClosedLine, locatordata.fHeelLineList)
-
-        ## Draw a text "Foot"
-        pos = om.MPoint( 0.0, 0.0, 0.0 )  ## Position of the text
-        textColor = om.MColor( (0.1, 0.8, 0.8, 1.0) )  ## Text color
-
-        drawManager.setColor( textColor )
-        drawManager.setFontSize( omr.MUIDrawManager.kSmallFontSize )
-        drawManager.text(pos, "Footprint", omr.MUIDrawManager.kCenter )
+        drawManager.line(locatordata.fsourcePt, locatordata.faimPt)
 
         drawManager.endDrawable()
 
-    def getMultiplier(self, objPath):
-        ## Retrieve value of the size attribute from the node
-        footprintNode = objPath.node()
-        plug = om.MPlug(footprintNode, footPrint.size)
-        if not plug.isNull:
-            sizeVal = plug.asMDistance()
-            return sizeVal.asCentimeters()
-
-        return 1.0
 
     def getPoints(self, objPath):
         ## Retrieve value of the source point attribute from the node
@@ -269,7 +150,7 @@ class footPrintDrawOverride(omr.MPxDrawOverride):
         return 1.0
 
 def initializePlugin(obj):
-    plugin = om.MFnPlugin(obj, "Autodesk", "3.0", "Any")
+    plugin = om.MFnPlugin(obj, "Dmitrii Shevchenko", "1.0", "Any")
 
     try:
         plugin.registerNode("footPrint", footPrint.id, footPrint.creator, footPrint.initialize, om.MPxNode.kLocatorNode, footPrint.drawDbClassification)
--- dsDrawVector/tests/drawVectorTest.py
+++ dsDrawVector/tests/drawVectorTest.py
@@ -6,3 +6,11 @@ mc.loadPlugin('C:\Users\dmitris\Documents\maya\scripts\dsNodes\dsDrawVector\plug
 
 ###### Test #########
 #Basic test
+sourceObj = mc.polyCube()[0]
+aimObj = mc.polyCube()[0]
+
+vectorNode = mc.createNode('footPrint')
+mc.connectAttr(sourceObj + '.t', vectorNode + '.sp')
+mc.connectAttr(aimObj + '.t', vectorNode + '.ap')
+
+mc.move( 2, 5, 10, aimObj)
